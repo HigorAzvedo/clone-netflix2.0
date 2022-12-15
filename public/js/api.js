4f9b8898e3ec13281e743cb016c8bd8b943ea8b0
HigorAzvedo <106608190+HigorAzvedo@users.noreply.github.com>
2022-12-15 20:51:05 -0300

    novo projeto

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "675d92710ac2020c29405f0951051703"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
